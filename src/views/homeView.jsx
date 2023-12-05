362f56d2f69344f98f1e1d4c70371fd92a3e65e5
George Yandem <gyandem@kth.se>
2023-12-05 15:44:13 +0100

    new files name

--- src/views/homeView.jsx
+++ src/views/homeView.jsx
@@ -12,3 +12,16 @@ function HomeView(props){
 }
 export default HomeView;
 
+/*function Test(props){
+  function backACB(evt){
+    console.log("back button works");
+        window.location.hash="/home";
+  }
+    return (
+        <div>
+          <button onClick={backACB}>the test view works</button>
+        </div>
+);
+}
+
+export default Test;*/
