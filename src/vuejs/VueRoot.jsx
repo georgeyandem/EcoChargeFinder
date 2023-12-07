e26389fa2fc5493bfc525bae810a3687b1fd5294
George Yandem <gyandem@kth.se>
2023-12-07 14:08:40 +0100

    VueRoot

--- src/vuejs/VueRoot.jsx
+++ src/vuejs/VueRoot.jsx
@@ -1,6 +1,8 @@
 //import Sidebar from "./sidebarPresenter.jsx";
 //import Search from "./searchPresenter.jsx";
 import Home from "./homePresenter.jsx";
+import About from "./aboutPresenter.jsx";
+import Search from "./searchPresenter.jsx";
 
 
 import {createRouter, RouterView, createWebHashHistory} from "vue-router";
@@ -13,17 +15,24 @@ export function makeRouter(model) {
                 path: "/",
                 component: <Home model={model} />
             },
-    
+            {
+              path: "/about",
+              component: <About model={model} />
+            },
+            {
+              path: "/search",
+              component: <Search model={model} />
+            },
         ]});
 }
 
 export function VueRoot(props){ 
 
-  if (props.model.ready){  //props.model.ready // make it (1===0) to view else status
+  if (1===1){  //props.model.ready // make it (1===0) to view else status
   return  ( 
     <div class="flex_parent">
 
-      <div><Home model ={props.model}/></div>
+      {/*<div><Home model ={props.model}/></div>*/}
       <div class="main_content"> <RouterView /> </div>
 
     </div>
