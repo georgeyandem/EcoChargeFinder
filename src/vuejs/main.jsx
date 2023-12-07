a16d680ecdda493fbc88f813aecb285e5bac9464
George Yandem <gyandem@kth.se>
2023-12-07 14:18:01 +0100

    main.jsx

--- src/vuejs/main.jsx
+++ src/vuejs/main.jsx
@@ -1,37 +1,41 @@
-// this file is same as index.jsx in LAB from "./src/vuejs/index.jsx"
-
-import "/src/teacherFetch.js"; // protection against fetch() in infinite re-render
-//import "/src/firebaseModel.js";
+import "/src/teacherFetch.js"; 
+//import "/src/firebaseModel.js";     //  3
 
 // (1) ------------ application state (model) -----------
 import model from "/src/EvModel.js";
 
-import { reactive, createApp } from "vue";
-const reactiveModel = reactive(model);
+// uncomment to make the app update when the model changes.
+
+import { reactive } from "vue";
+const reactiveModel= reactive(model);
 
 
-//import {createApp, watch} from "vue";
+// then use reactiveModel instead of model below!
 
-import { VueRoot } from "./VueRoot.jsx";
-//import {VueRoot, makeRouter} from "./VueRoot.jsx";
+// (2) ----------  display (mount) the root component in the browser page. Pass model(1) as prop. ---------
+// http://localhost:8080/vue.html
 
-//import connectToFirebase, { readFromFirebase } from "../firebaseModel.js";
-const app = createApp(<VueRoot model={reactiveModel} />);
+import {createApp, h} from "vue";
+window.React= {createElement:h};  // needed in the lab because it works with both React and Vue
 
-app.use(makeRouter(reactiveModel));
+import {VueRoot, makeRouter} from "./VueRoot.jsx";      // makeRouter 3
+//import connectToFirebase, { readFromFirebase } from "../firebaseModel.js";  // 3 symbol
+const app= createApp(<VueRoot model={reactiveModel} />);
 
-app.mount("#root"); // WARNNING: LAB INSTRUCTIONS => mounts the app in the page DIV with the id "root"
+app.use(makeRouter(reactiveModel)); // 3.3 use reactiveModel instead of model below!
+
+app.mount('#root'); // mounts the app in the page DIV with the id "root"
 // to see the DIV, look at vue.html in the developer tools Sources
 // vue.html, with the content <div id="root"></div> is configured in vite.config.js
 
 // ------ for debug purposes ----------
 //window.myModel= model;             // make the model available in the Console
-window.myModel= reactiveModel;  
+//window.myModel= reactiveModel;  
 
 // Empty search
 //reactiveModel.doSearch({});
 
-//Connect model to realtime database
-import {watch} from "vue";
+// Connect model to realtime database      3
+//import {watch} from "vue";
 //To make sure that the model is read from firebase when the app starts, as well as saved whenever it changes
 //connectToFirebase(reactiveModel, watch); 
