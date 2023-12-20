c276cb9b0c63d95f976d2c447f52b953d8116bcb
Adalet Adiljan <adalat@kth.se>
2023-12-20 04:48:42 +0100

    pages for the contact and description information

---
+++ contactView.jsx
@@ -0,0 +1,10 @@
+function ContactView() {
+    return (
+      <div>
+        <h1>Contact Us</h1>
+        {/* Add your contact information and form here */}
+      </div>
+    );
+  }
+  
+  export default ContactView;
---
+++ descriptionView.jsx
@@ -0,0 +1,14 @@
+function descriptionView() {
+    return (
+      <div>
+        <h1>App Description</h1>
+        <p>
+          This app is designed to help users [provide a brief description of what the app does].
+          {/* Add more detailed description as needed */}
+        </p>
+        {/* Add any additional sections or content related to the app description */}
+      </div>
+    );
+  }
+  
+  export default descriptionView;
