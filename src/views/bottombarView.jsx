e5d6ce7cbbeb7f4d2e67043760c0c65745ddbcda
SHAEMAAToma <shaemaa@kth.se>
2023-12-22 14:20:59 +0100

    BottombarView

--- src/views/bottombarView.jsx
+++ src/views/bottombarView.jsx
@@ -1,6 +1,5 @@
-import "/src/style.css";
-
 function BottombarView() {
+    const router = useRouter();
     return (
       <footer>
         <div>
@@ -11,7 +10,27 @@ function BottombarView() {
         </div>
       </footer>
     );
+  
+  const navigateToTerms = () => {
+    router.push("/terms");
+  };
+
+  const navigateToPrivacyPolicy = () => {
+    router.push("/privacy");
+  };
+
+  return (
+    <footer>
+      <div>
+        &copy; {new Date().getFullYear()} EcoChargeFinder. All rights reserved.
+      </div>
+      <div>
+        <button onClick={navigateToTerms}>Terms of Service</button> |{" "}
+        <button onClick={navigateToPrivacyPolicy}>Privacy Policy</button>
+      </div>
+    </footer>
+  );
+
   }
   
   export default BottombarView;
-  
