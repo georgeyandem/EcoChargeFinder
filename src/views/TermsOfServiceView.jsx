f71146bb1fc7c3f6f0dcbd9d8a0beb45441c472c
SHAEMAAToma <shaemaa@kth.se>
2023-12-22 13:54:55 +0100

    Add TermsOfServiceView

---
+++ src/views/TermsOfServiceView.jsx
@@ -0,0 +1,12 @@
+export default function TermsOfServiceView() {
+    return (
+      <div>
+        <h1>Terms of Service</h1>
+        <p>
+          Welcome to EcoChargeFinder! These terms of service outline the rules
+          and regulations for the use of our platform.
+        </p>
+      </div>
+    );
+  }
+  
