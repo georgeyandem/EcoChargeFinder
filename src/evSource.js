59ca13223040b56d3cfabc6f91ba07af077e18d3
louai20 <alayoubi@kth.se>
2023-12-22 16:27:11 +0100

    fixes favorite station

--- src/evSource.js
+++ src/evSource.js
@@ -3,6 +3,8 @@ import {
   API_KEY,
   API_KEY_TOMTOM,
   BASE_URL_TOMTOM,
+  API_KEY_Image,
+  BASE_URL_Image,
 } from "./apiConfig.js";
 
 function responseACB(response) {
@@ -57,24 +59,47 @@ export function locationDetails(chargingStationAddress) {
   const url =
     BASE_URL_TOMTOM +
     chargingStationAddress +
-    `.json?key=${apiKey}&countrySet=SE&limit=1`;
+    `.json?key=${API_KEY_TOMTOM}&countrySet=SE&limit=1`;
   // Make a GET request to retrieve charging station details
   fetch(url)
     .then(responseACB)
-    .then((data) => {
-      // Handle the response data here
-      console.log(data);
-      // Extract and display relevant details about the charging station
-    })
     .catch((error) => {
       console.error("Error fetching charging station data:", error);
     });
 }
 
+// get random evo chrage images
+export function getImage() {
+  const searchTerm = "charging station"; // Replace with your search query
+  const url =
+    BASE_URL_Image +
+    `api_key=${API_KEY_Image}&tags=${searchTerm}&format=json&nojsoncallback=1`;
+  fetch(url, {
+    method: "GET",
+  })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Network response was not ok.");
+      }
+      return response.json();
+    })
+    .then((data) => {
+      const photos = data.photos.photo;
+      const randomIndex = Math.floor(Math.random() * photos.length);
+      const randomPhoto = photos[randomIndex];
+
+      const imageUrl = `https://live.staticflickr.com/${randomPhoto.server}/${randomPhoto.id}_${randomPhoto.secret}.jpg`;
+      console.log(imageUrl);
+    })
+    .catch((error) => {
+      console.error("Error fetching data:", error);
+    });
+}
+
 /*
-// Function to fetch a random image of charging stations
+
 const apiKey3 = "b2051100ae29c7cf058ec9e06e91cd0d";
-const searchTerm = "charging station"; // Replace with your search query
+
 const flickrEndpoint = `https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey3}&tags=${searchTerm}&format=json&nojsoncallback=1`;
 
 // Fetch images from Flickr API
