ea263dc941e9870f2f58da5dbfab9ea84535da48
George Yandem <gyandem@kth.se>
2023-12-22 15:25:39 +0100

    search button

--- src/views/topbarView.jsx
+++ src/views/topbarView.jsx
@@ -85,7 +85,7 @@ function TopbarView(props) {
               <li>
                 <a
                   onClick={searchACB}
-                  class="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                  class="block py-2 px-3 md:p-0 text-gray-100 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
                 >
                   Search for EcoCharger
                 </a>
